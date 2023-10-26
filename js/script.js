84daee39e2c8a8d71c339c05488b6b9cc04998d4
alilein1990 <alina.leinweber@gmx.de>
2023-10-26 20:58:42 +0200

    add key enter to add the item to <ol>

--- js/script.js
+++ js/script.js
@@ -2,37 +2,42 @@
 function newItem() {
 
     //1. Adding a new item to the list of items: 
-    let li = $('.container').append("li");
-    let inputValue = $("#input").val();
-    li.append(document.createTextNode(inputValue));
+    let li = $('<li></li>');
+    let inputValue = $('#input').val();
+    li.text(inputValue);
 
+    // append li to ol and clear field after
     if (inputValue === '') {
-        alert("You must write something!");
+        alert('You must write something!');
     } else {
-        let list = $('#list');
-        list.append(li);
-        $("#input").val() = '';
+        $('#list').append(li);
+        $("#input").val('')
     }
 
-    //2. Crossing out an item from the list of items:
-    function crossOut() {
-        li.classList.toggle("strike");
-    }
+    //add class strike on double click
+    li.on("dblclick", function crossOut() {
+        li.toggleClass('strike');
+    });
 
-    li.addEventListener("dblclick", crossOut);
+    //add  delete button "X": 
+    let crossOutButton = $('<crossOutButton>X</crossOutButton>');
+    li.append(crossOutButton);
 
-    //3(i). Adding the delete button "X": 
-    let crossOutButton = document.createElement("crossOutButton");
-    crossOutButton.appendChild(document.createTextNode("X"));
-    li.appendChild(crossOutButton);
+    // add class delete to li on click
+    crossOutButton.on('click', function deleteListItem() {
+        li.addClass('delete')
+    });
 
-    crossOutButton.addEventListener("click", deleteListItem);
-    //3(ii). Adding CLASS DELETE (DISPLAY: NONE) from the css:
-    function deleteListItem() {
-        li.classList.add("delete")
-    }
     // 4. Reordering the items: 
     $('#list').sortable();
-
 }
 
+// add enter as key to add an item 
+$(document).ready(function() {
+    $('#input').on('keydown', function(event) {
+        if (event.key === 'Enter'){
+            event.preventDefault();
+            newItem();
+        }
+    });
+});
